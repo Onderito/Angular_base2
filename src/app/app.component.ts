8f6fa2cab020aa8d247fb01337f48f28b32ffc8b
Onderito <ulas.onder@outlook.fr>
2024-04-03 16:03:13 +0200

    main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CocktailListComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
